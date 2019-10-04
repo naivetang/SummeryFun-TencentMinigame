63a7909a72094c4bc7476b7c0bb7de66a2b0ab60
haobotang <haobotang@tencent.com>
2019-10-04 10:28:09 +0000

    2d导航

--- Unity/Assets/Navigation2D/Plugins/NavMeshPath2D.cs
+++
@@ -1,9 +0,0 @@
-﻿// agent.CalculatePath returns a 3D path, but we need a 2D path.
-using UnityEngine;
-using UnityEngine.AI;
-
-public class NavMeshPath2D
-{
-    public Vector2[] corners;
-    public NavMeshPathStatus status;
-}
