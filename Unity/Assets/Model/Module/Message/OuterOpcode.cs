c0259d3cc41caf4674cd1272ddcb0be1ecc626f4
haobotang <haobotang@tencent.com>
2019-10-05 11:40:19 +0000

    协议

--- Unity/Assets/Model/Module/Message/OuterOpcode.cs
+++ Unity/Assets/Model/Module/Message/OuterOpcode.cs
@@ -4,14 +4,14 @@ namespace ETModel
 	[Message(OuterOpcode.LoginReq)]
 	public partial class LoginReq : IRequest {}
 
+	[Message(OuterOpcode.LoginRsp)]
+	public partial class LoginRsp : IResponse {}
+
 	[Message(OuterOpcode.RegisterReq)]
 	public partial class RegisterReq : IRequest {}
 
 	[Message(OuterOpcode.RegisterRsp)]
 	public partial class RegisterRsp : IResponse {}
-
-	[Message(OuterOpcode.LoginRsp)]
-	public partial class LoginRsp : IResponse {}
 
 	[Message(OuterOpcode.TaskQueryReq)]
 	public partial class TaskQueryReq : IRequest {}
@@ -20,11 +20,20 @@ namespace ETModel
 	[Message(OuterOpcode.TaskQueryRsp)]
 	public partial class TaskQueryRsp : IResponse {}
 
+// repeated int32 get_tasks = 2; // 现在接取到的任务
 	[Message(OuterOpcode.TaskUpdateReq)]
 	public partial class TaskUpdateReq : IRequest {}
 
+// 刚刚完成的任务
+// repeated int32 get_task_ids = 2;
 	[Message(OuterOpcode.TaskUpdateRsp)]
 	public partial class TaskUpdateRsp : IResponse {}
+
+	[Message(OuterOpcode.EraseRoleReq)]
+	public partial class EraseRoleReq : IRequest {}
+
+	[Message(OuterOpcode.EraseRoleRsp)]
+	public partial class EraseRoleRsp : IResponse {}
 
 	[Message(OuterOpcode.C2M_TestRequest)]
 	public partial class C2M_TestRequest : IActorLocationRequest {}
@@ -86,20 +95,22 @@ namespace ETModel
 		 public const ushort TaskQueryRsp = 106;
 		 public const ushort TaskUpdateReq = 107;
 		 public const ushort TaskUpdateRsp = 108;
-		 public const ushort C2M_TestRequest = 109;
-		 public const ushort M2C_TestResponse = 110;
-		 public const ushort Actor_TransferRequest = 111;
-		 public const ushort Actor_TransferResponse = 112;
-		 public const ushort C2G_EnterMap = 113;
-		 public const ushort G2C_EnterMap = 114;
-		 public const ushort UnitInfo = 115;
-		 public const ushort M2C_CreateUnits = 116;
-		 public const ushort Frame_ClickMap = 117;
-		 public const ushort M2C_PathfindingResult = 118;
-		 public const ushort C2R_Ping = 119;
-		 public const ushort R2C_Ping = 120;
-		 public const ushort G2C_Test = 121;
-		 public const ushort C2M_Reload = 122;
-		 public const ushort M2C_Reload = 123;
+		 public const ushort EraseRoleReq = 109;
+		 public const ushort EraseRoleRsp = 110;
+		 public const ushort C2M_TestRequest = 111;
+		 public const ushort M2C_TestResponse = 112;
+		 public const ushort Actor_TransferRequest = 113;
+		 public const ushort Actor_TransferResponse = 114;
+		 public const ushort C2G_EnterMap = 115;
+		 public const ushort G2C_EnterMap = 116;
+		 public const ushort UnitInfo = 117;
+		 public const ushort M2C_CreateUnits = 118;
+		 public const ushort Frame_ClickMap = 119;
+		 public const ushort M2C_PathfindingResult = 120;
+		 public const ushort C2R_Ping = 121;
+		 public const ushort R2C_Ping = 122;
+		 public const ushort G2C_Test = 123;
+		 public const ushort C2M_Reload = 124;
+		 public const ushort M2C_Reload = 125;
 	}
 }
