e05190d6292f72dc0bcd483912e37117b761ec95
breamzhao <breamzhao@tencent.com>
2019-10-05 15:27:55 +0000

    猪关

--- Unity/Assets/Model/UI/UIPigScene/UIPigSceneComponent.cs
+++ Unity/Assets/Model/UI/UIPigScene/UIPigSceneComponent.cs
@@ -24,6 +24,20 @@ namespace ETModel
     {
         private GameObject drawscene2;
 
+        private GameObject child1;
+
+        private GameObject child2;
+
+        private GameObject youzi;
+
+        private GameObject hine;
+
+        private GameObject stayHine;
+
+        private Vector2 inityouziPosition;
+
+        private GameObject cancel;
+
         private int triggerId = 3003;
 
         public void Awake()
@@ -32,10 +46,42 @@ namespace ETModel
 
             this.drawscene2 = rc.Get<GameObject>("drawscene2");
 
-            Game.EventSystem.Run<int>(EventIdType.CompleteTask, this.triggerId);
+            this.hine = rc.Get<GameObject>("hine");
 
-            this.CollectAndShow();
+            this.child1 = rc.Get<GameObject>("child1");
+
+            this.child2 = rc.Get<GameObject>("child2");
+
+            this.youzi = rc.Get<GameObject>("youzi");
+
+            this.inityouziPosition = this.youzi.transform.localPosition;
+
+            this.cancel = rc.Get<GameObject>("Cancel");
+
+            this.init();
+            
+
+            
+
+            //this.CollectAndShow();
            
+        }
+
+        void init()
+        {
+            this.child2.SetActive(false);
+
+            this.cancel.GetComponent<Button>().onClick.AddListener(this.Close);
+
+            RegistColliderTrriger();
+
+            RegistDrageEvent();
+                       
+        }
+
+        void AddListener()
+        {
+
         }
 
         async ETVoid CollectAndShow()
@@ -68,7 +114,133 @@ namespace ETModel
         {
 
         }
+        
+        void RegistDrageEvent()
+        {
+            var darageable = this.youzi.GetComponent<UIDragable>();
 
+
+            darageable.RegistOnBeginDrag(this.BeginDrageyouzi);
+            darageable.RegistOnDrag(this.Drageingyouzi);
+            darageable.RegistOnEndDrag(this.EndDrageyouzi);
+
+            darageable.RegisterPointDown(this.DragPointDown);
+            darageable.RegisterPointUp(this.DragPointUp);
+        }
+
+        void DragPointDown(PointerEventData p)
+        {
+            //Log.Info("down");
+
+            this.youzi.transform.DOScale(Vector3.one * 1.1f, 0.15f);
+        }
+
+        void DragPointUp(PointerEventData p)
+        {
+            //Log.Info("up");
+
+            this.youzi.transform.DOScale(Vector3.one, 0.15f);
+           
+        }
+
+        void BeginDrageyouzi(PointerEventData p)
+        {
+            //Log.Info("begin drage");
+
+            this.youzi.GetComponent<Image>().CrossFadeAlpha(1f, 0.1f, true);
+                        
+        }
+
+        void Drageingyouzi(PointerEventData p)
+        {
+            this.child1.SetActive(false);
+
+            this.child2.GetComponent<CanvasGroup>().DOFade(0.4f, 0);
+
+            this.child2.SetActive(true);
+                        
+            //Log.Info("draging");
+        }
+
+        void EndDrageyouzi(PointerEventData p)
+        {
+            //Log.Info("end draging");
+
+            // 解题成功
+            if (this.stayHine != null)
+            {
+                this.success();
+            }
+            else
+            {
+                this.fail();
+            }
+
+        }
+
+        //注册柚子进入Hine状态
+        void RegistColliderTrriger()
+        {
+            this.hine.GetComponent<UIColliderTrigger>().RegistOnTriggerEnter2D((p) =>
+            {
+                HineTriggerEnter2D(this.hine, p);
+            });
+
+             this.hine.GetComponent<UIColliderTrigger>().RegistOnTriggerExit2D((p) =>
+             {
+                 HineTriggerExit2D(this.hine, p);
+             });
+
+             this.hine.GetComponent<UIColliderTrigger>().RegistOnTriggerStay2D((p) =>
+             {
+                 HineTriggerStay2D(this.hine, p);
+             });
+            
+        }
+
+
+        void HineTriggerEnter2D(GameObject gameobject, Collider2D collider2D)
+        {
+            gameobject.GetComponent<CanvasGroup>().DOFade(0.4f, 0);
+                       
+            this.stayHine = gameobject;
+        }
+
+        void HineTriggerExit2D(GameObject gameobject, Collider2D collider2D)
+        {
+            gameobject.GetComponent<CanvasGroup>().DOFade(0f, 0);
+
+            this.stayHine = null;
+        }
+
+
+        void HineTriggerStay2D(GameObject hine, Collider2D collider2D)
+        {
+
+        }
+
+        async ETVoid success()
+        {
+            this.child1.SetActive(false);
+
+            this.child2.SetActive(true);
+
+            Game.EventSystem.Run<int>(EventIdType.CompleteTask, this.triggerId);
+
+            this.CollectAndShow();
+        }
+
+        void fail()
+        {
+            this.child1.SetActive(true);
+
+            this.child2.GetComponent<CanvasGroup>().DOFade(1, 0);
+
+            this.child2.SetActive(false);
+
+            this.youzi.transform.localPosition = this.inityouziPosition;
+
+        }
 
         async ETVoid CollectToBook()
         {
